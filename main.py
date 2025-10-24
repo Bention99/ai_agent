9444e46307fb02c49099264556b66406527fb473
Bention99 <benoitwyss0@gmail.com>
2025-10-24 15:28:52 +0200

    working prompts

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv("gem.env")
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(model=model, contents=contents)
+
+    print(response.text)
+    usage = prompt.usage_metadata
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
 
 
 if __name__ == "__main__":
