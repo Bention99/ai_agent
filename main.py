9388a88776c035cd35e48de2f9c7c3145267db30
Bention99 <benoitwyss0@gmail.com>
2025-10-24 18:22:47 +0200

    added --verbose argument to get infos about prompt if wished

--- main.py
+++ main.py
@@ -2,37 +2,52 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def inputchecker(input):
     input_string = ""
+    verbose = False
     if len(input) < 2:
         print("Error: prompt not provided.")
         sys.exit(1)
     else:
         for i in range(1, len(input)):
-            input_string += input[i] + " "
-        print(f"input is: {input_string}")
-        return input_string
+            if input[i] == "--verbose":
+                verbose = True
+            else:
+                input_string += input[i] + " "
+        return input_string, verbose
 
-def ask_gemini(prompt):
+def ask_gemini(messages):
     load_dotenv("gem.env")
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
 
-    return client.models.generate_content(model=model, contents=prompt)
+    return client.models.generate_content(model=model, contents=messages)
 
 def main():
 
     user_input = sys.argv
-    prompt = inputchecker(user_input)
+    receiver = inputchecker(user_input)
+    prompt = receiver[0]
+    verbose = receiver[1]
 
-    response = ask_gemini(prompt)
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = ask_gemini(messages)
+    usage = response.usage_metadata
+
+    if verbose == True:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
 
     print(response.text)
-    usage = response.usage_metadata
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    
 
+    
 if __name__ == "__main__":
     main()
