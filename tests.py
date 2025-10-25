822d86a6db9648224e141606976c75a02a46a585
Bention99 <benoitwyss0@gmail.com>
2025-10-25 15:33:31 +0200

    function write file added

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 from functions.get_files_info import *
 from functions.get_files_content import *
+from functions.write_file import *
 
 def test():
+
+    # get_files_info tests
+
     '''result = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(result)
@@ -19,10 +23,12 @@ def test():
     print("Result for '../' directory:")
     print(result)'''
 
+    # get_files_content tests
+
     '''result = get_files_content("calculator", "lorem.txt")
     print(result)'''
 
-    result = get_files_content("calculator", "main.py")
+    '''result = get_files_content("calculator", "main.py")
     print(result)
 
     result = get_files_content("calculator", "pkg/calculator.py")
@@ -32,8 +38,18 @@ def test():
     print(result)
 
     result = get_files_content("calculator", "pkg/does_not_exist.py")
+    print(result)'''
+
+    # write_file tests
+
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
 
 
 if __name__ == "__main__":
